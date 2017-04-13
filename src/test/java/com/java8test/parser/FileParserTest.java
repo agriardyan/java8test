ef58ea6ef1b554807d60b18aa6e32e453c009207
agriardyan <superbro.ardyan@gmail.com>
2017-04-13 16:02:47 +0700

    add testing

--- src/test/java/com/java8test/parser/FileParserTest.java
+++ src/test/java/com/java8test/parser/FileParserTest.java
@@ -11,37 +11,37 @@ import static org.junit.Assert.*;
  */
 public class FileParserTest {
 
+//    @Test
+//    public void parse() {
+//        assertTrue(true);
+//    }
+
     @Test
-    public void parse() {
-        assertTrue(true);
+    public void parse() throws Exception {
+        String filename = "resource.txt";
+        List<String> parse = FileParser.parse(filename);
+        assertNotNull("It should not null", parse);
+        assertTrue(parse.size() > 0);
     }
 
-//    @Test
-//    public void parse() throws Exception {
-//        String filename = "resource.txt";
-//        List<String> parse = FileParser.parse(filename);
-//        assertNotNull("It should not null", parse);
-//        assertTrue(parse.size() > 0);
-//    }
-//
-//    @Test
-//    public void parse1() throws Exception {
-//        String filename = "resource.txt";
-//        List<String> parse = FileParser.parse(filename);
-//        List<String> parse1 = FileParser.parse(filename, n -> n.equals("Tech"));
-//        assertNotNull("It should not null", parse1);
-//        assertTrue(parse.size() >= parse1.size());
-//    }
-//
-//    @Test
-//    public void write() throws Exception {
-//        String filename = "resource.txt";
-//        String content = "Test|||This is a content for testing.";
-//        List<String> parse = FileParser.parse(filename);
-//        FileParser.write(filename,content);
-//        List<String> parse1 = FileParser.parse(filename);
-//        assertNotNull("It should not null", parse1);
-//        assertTrue(parse1.size() > parse.size());
-//    }
+    @Test
+    public void parse1() throws Exception {
+        String filename = "resource.txt";
+        List<String> parse = FileParser.parse(filename);
+        List<String> parse1 = FileParser.parse(filename, n -> n.equals("Tech"));
+        assertNotNull("It should not null", parse1);
+        assertTrue(parse.size() >= parse1.size());
+    }
+
+    @Test
+    public void write() throws Exception {
+        String filename = "resource.txt";
+        String content = "Test|||This is a content for testing.";
+        List<String> parse = FileParser.parse(filename);
+        FileParser.write(filename,content);
+        List<String> parse1 = FileParser.parse(filename);
+        assertNotNull("It should not null", parse1);
+        assertTrue(parse1.size() > parse.size());
+    }
 
 }
